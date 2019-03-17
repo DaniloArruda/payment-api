26966f8fe13302be5245692ee54f2a463617dee2
unknown <da.ni.lo_arruda@outlook.com>
2019-03-17 11:44:29 -0300

    Retirado o caminho 'pagamento-api'

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ mongoose.connect("mongodb+srv://danilo:cuia1234@pagamento-cluster-vhk3r.mongodb.
     .catch(err => console.error('Something went wrong', err));
 requireDir("./src/model");
 
-app.use("/pagamento-api", require("./src/routes"));
+app.use("/", require("./src/routes"));
 
 let port = process.env.PORT;
 if (port == null || port == "") {
